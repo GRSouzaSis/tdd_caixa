5280f507e9859883e0e78e52b0804b83d08c5261
Givaldo Souza <givaldo.souza@opah.com.br>
2021-09-26 11:29:02 -0300

    init testes

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: 'motty.db.elephantsql.com (motty-01)',
+  username: 'bilbtxpl',
+  password: 'qHTD-5UcFimi1Bl71o3fesVX1IBoiyRh',
+  database: 'bilbtxpl',
+  dialect: 'postgres',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
